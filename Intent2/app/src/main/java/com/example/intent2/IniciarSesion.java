461958c4b1e9e7333b2ce9ae89c552326fe03aaf
Soto02 <asotfer636@g.educaand.es>
2023-11-06 20:15:10 +0100

    Iniciar Sesion y registrar

--- Intent2/app/src/main/java/com/example/intent2/IniciarSesion.java
+++ Intent2/app/src/main/java/com/example/intent2/IniciarSesion.java
@@ -11,11 +11,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class IniciarSesion extends AppCompatActivity {
 
     EditText nombreEdit, contrasenaEdit;
     Button login, registrar;
+    SharedPreferences preferencias;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,14 +29,26 @@ public class IniciarSesion extends AppCompatActivity {
         login = (Button) findViewById(R.id.btnIniciarSesion);
         registrar = (Button) findViewById(R.id.btnRegistrarse);
 
+        preferencias = getSharedPreferences("user", MODE_PRIVATE);
+
         login.setBackgroundColor(getResources().getColor(R.color.grey));
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                guardarPreferencias();
-                Intent intent = new Intent(IniciarSesion.this, PaginaPrincipal.class);
-                startActivity(intent);
-                finish();
+                String nombreGuardado = preferencias.getString("nombre", "");
+                String contrasenaGuardada = preferencias.getString("contrasena", "");
+
+                String nombre = nombreEdit.getText().toString();
+                String contrasena = contrasenaEdit.getText().toString();
+
+                if(nombre.equals(nombreGuardado) && contrasena.equals(contrasenaGuardada)) {
+                    Toast.makeText(IniciarSesion.this, "Inicio de sesión exitoso", Toast.LENGTH_SHORT);
+                    Intent intent = new Intent(IniciarSesion.this, PaginaPrincipal.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(IniciarSesion.this, "Ha habido un error", Toast.LENGTH_SHORT);
+                }
+
             }
         });
 
@@ -42,11 +56,20 @@ public class IniciarSesion extends AppCompatActivity {
         registrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                cargarPreferencias();
+                String nombre = nombreEdit.getText().toString();
+                String contrasena = contrasenaEdit.getText().toString();
+
+                SharedPreferences.Editor editor = preferencias.edit();
+                editor.putString("nombre", nombre);
+                editor.putString("contrasena", contrasena);
+                editor.apply();
+
+                Toast.makeText(IniciarSesion.this, "Usuario registrado", Toast.LENGTH_SHORT).show();
             }
         });
     }
-    private void guardarPreferencias() {
+}
+    /*private void guardarPreferencias() {
         SharedPreferences preferencias = getSharedPreferences("credenciales", Context.MODE_PRIVATE);
 
         String user = nombreEdit.getText().toString();
@@ -57,12 +80,17 @@ public class IniciarSesion extends AppCompatActivity {
         editor.putString("pass", pass);
 
         editor.commit();
-    }
-    private void cargarPreferencias() {
+    }*/
+    /*private void cargarPreferencias() {
         SharedPreferences preferencias = getSharedPreferences("credenciales", Context.MODE_PRIVATE);
 
         String user = preferencias.getString("user", "No existe");
         String pass = preferencias.getString("pass", "No existe");
-    }
+    }*/
 
-}
+
+/*
+Intent intent = new Intent(IniciarSesion.this, PaginaPrincipal.class);
+                startActivity(intent);
+                finish();
+ */
